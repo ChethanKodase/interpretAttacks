7695eeefc1b732367882b6a0c1c3617f87a477da
ChethanKodase <chethankr27@gmail.com>
2026-09-10 12:58:30 +0200

    updates

--- qwen/QwenUntargeted_BSA.py
+++ qwen/QwenUntargeted_BSA.py
@@ -36,7 +36,7 @@ for ATTACK_SAMPLE in $(seq 51 100); do
     python qwen/QwenUntargeted_BSA.py --attck_type bsa --desired_norm_l_inf 0.002 --learningRate 0.001 --num_steps 1000 --attackSample $ATTACK_SAMPLE
 done
 
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=0
 conda deactivate
 cd interpretAttacks/
 conda activate vlmAttack
